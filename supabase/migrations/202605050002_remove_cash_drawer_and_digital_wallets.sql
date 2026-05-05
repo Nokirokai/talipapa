d390b62c9138f331b7ce2b20f589e0a6b3bc0e11
Kael bruh <146412754+Nokirokai@users.noreply.github.com>
2026-05-05 18:46:30 +0800

    Delete supabase directory

--- supabase/migrations/202605050002_remove_cash_drawer_and_digital_wallets.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists public.cash_drawer_sessions;
-
-alter table public.transactions
-  drop constraint if exists transactions_payment_method_check;
-
-alter table public.transactions
-  add constraint transactions_payment_method_check
-  check (payment_method in ('cash','utang'));
